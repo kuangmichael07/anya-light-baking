d1c7de1138c9c35a5ed7e57c7c58546aed1d7352
Michael Kuang <kgz406@gmail.com>
2016-09-09 12:52:59 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -92,7 +92,7 @@ $("i").on("click", function(){
 //         $(".map").hide();
 		// document.querySelector('#anya-location').css("");
 		// $('#anya-location').load("resources/google-map.html");
-		$("#anya-location").hide();
+		$("#anya-location").show();
 		initMap();
 
       });
